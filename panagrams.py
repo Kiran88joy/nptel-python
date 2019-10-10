27ddd63dc61bce8b1ce2c1e8e37787b174a1c2f7
Kiran_Joy <51417521+Kiran88joy@users.noreply.github.com>
2019-10-10 19:01:15 +0530

    Update panagrams.py
    slick

--- panagrams.py
+++ panagrams.py
@@ -4,7 +4,4 @@ x1='a b c d e f g h i j k l m n o p q r s t u v w x y z 0'.split()
 for i in x :
 	if i in x1 :
 		x1.remove(i)
-if x1[0]=='0' :
-	print('YES',end="")
-else :
-	print('NO',end="")
+print("YES" if x1[]0=='0' else "NO",end="")
